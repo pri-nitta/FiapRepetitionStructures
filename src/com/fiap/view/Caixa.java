eef144db7a05982de656e09919d8478debcaaaf2
Priscila Nitta <priscila.nitta@venturus.org.br>
2021-09-16 16:59:38 -0300

    first commit

---
+++ src/com/fiap/view/Caixa.java
@@ -0,0 +1,17 @@
+package com.fiap.view;
+
+public class Caixa {
+
+	public static void main(String[] args) {
+		
+		int qtdProdutos = 5;
+		int registro = 0;
+		
+		while (registro < qtdProdutos) {
+			registro++;
+			System.out.println("O produto número " + registro + " foi registrado.");
+		}
+
+	}
+
+}
