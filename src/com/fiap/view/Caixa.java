3ad41ffd399b1ca4bdb87acb07f1c53fc5968410
Priscila Nitta <priscila.nitta@venturus.org.br>
2021-09-16 17:08:33 -0300

    for

--- src/com/fiap/view/Caixa.java
+++ src/com/fiap/view/Caixa.java
@@ -12,12 +12,17 @@ public class Caixa {
 //			registro++;
 //			System.out.println("O produto número " + registro + " foi registrado.");
 //		}
+//		
+//		//executa antes e dpois verifica
+//		do {
+//			registro++;
+//			System.out.println("O Caixa registrou o produto " +registro);
+//		} while (registro < qtdProdutos);
 		
-		//executa antes e dpois verifica
-		do {
-			registro++;
-			System.out.println("O Caixa registrou o produto " +registro);
-		} while (registro < qtdProdutos);
+		//faz a mesma coisa, sem a variável registro
+		for (int i = 1; i <= qtdProdutos; i++) {
+			System.out.println("O caixa registrou o produto " +i);
+		}
 
 	}
 
