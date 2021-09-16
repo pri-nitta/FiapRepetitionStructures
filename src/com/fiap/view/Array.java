f7a596d0608d0662a340d3d74e4c00cb2709f619
Priscila Nitta <priscila.nitta@venturus.org.br>
2021-09-16 17:39:59 -0300

    object array

--- src/com/fiap/view/Array.java
+++ src/com/fiap/view/Array.java
@@ -21,7 +21,39 @@ public class Array {
 		
 		int valor2[] = new int[] {2,4,6,8,10};
 		System.out.println(valor2[0]);
-
+		
+		Produto[] produtos = new Produto[2];
+		
+		Produto prod1 = new Produto();
+		prod1.setNome("Chocolate");
+		prod1.setDescricao("Meio Amargo");
+		prod1.setValor(7);
+		
+		Produto prod2 = new Produto();
+		prod2.setNome("Chocolate");
+		prod2.setDescricao("Ao leite");
+		prod2.setValor(8);
+		
+		produtos[0] = prod1;
+		produtos[1] = prod2;
+		
+		for (int i=0; i < produtos.length; i++) {
+			System.out.println(produtos[i].toString());
+		}
+		
+		for (Produto prod: produtos) {
+			System.out.println(prod.toString());
+		}
+		
+		
+		//Array multidimensional
+		//10 corredores com 3 prateleiras cada
+		Produto[][] localizacaoProduto = new Produto[10][3];
+		
+		localizacaoProduto[2][1] = prod1;
+		localizacaoProduto[2][2] = prod2;
+		
+		System.out.println(localizacaoProduto[2][1].getNome() + " " + localizacaoProduto[2][1].getDescricao());
 	}
 
 }
