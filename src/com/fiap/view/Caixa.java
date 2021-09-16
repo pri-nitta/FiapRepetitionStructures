4c47d8840d860fc36ff7b73e6632c1af399c6597
Priscila Nitta <priscila.nitta@venturus.org.br>
2021-09-16 17:05:59 -0300

    do while

--- src/com/fiap/view/Caixa.java
+++ src/com/fiap/view/Caixa.java
@@ -7,10 +7,17 @@ public class Caixa {
 		int qtdProdutos = 5;
 		int registro = 0;
 		
-		while (registro < qtdProdutos) {
+//		// faz a checagem antes
+//		while (registro < qtdProdutos) {
+//			registro++;
+//			System.out.println("O produto número " + registro + " foi registrado.");
+//		}
+		
+		//executa antes e dpois verifica
+		do {
 			registro++;
-			System.out.println("O produto número " + registro + " foi registrado.");
-		}
+			System.out.println("O Caixa registrou o produto " +registro);
+		} while (registro < qtdProdutos);
 
 	}
 
