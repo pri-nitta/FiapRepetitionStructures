5932c33f56e0e6115c30b7dec9116f5ec748713a
Priscila Nitta <priscila.nitta@venturus.org.br>
2021-09-17 17:18:56 -0300

    string methods

--- src/com/fiap/view/Strings.java
+++ src/com/fiap/view/Strings.java
@@ -1,5 +1,7 @@
 package com.fiap.view;
 
+import java.util.Arrays;
+
 public class Strings {
 
 	public static void main(String[] args) {
@@ -41,7 +43,47 @@ public class Strings {
 		
 		mais += "!";
 		System.out.println(mais);
-
+		
+		System.out.println("+++++++++++++++++++++++++++++++++++++");
+		//comparando strings
+		String n1 = new String ("sono");
+		String n2 = new String("Sono");
+		String n3 = new String("sono");
+		
+		System.out.println(n1.equals(n2)); //false pq tem uma letra maiuscula
+		System.out.println(n1.equalsIgnoreCase(n2)); // true pq ignora as letras maiusculas
+		System.out.println(n1.equals(n3)); //true
+		
+		boolean teste = (n1 == n3);
+		System.out.println(teste); // false, pq alocação de memória é diferente
+		
+	
+		String n4 = "cansada";
+		String n5 = "cansada";
+		
+		teste = (n4 == n5);
+		System.out.println(teste); // true, pq como não foram instanciadas, estão armazenadas em um pool de memória
+		
+		String musica = new String("O mundo acaba hoje e eu estarei dançando");
+		
+		System.out.println("A quantidade de caracteres da frase é " + musica.length());
+		System.out.println("Começa com O? " + musica.startsWith("O"));
+		System.out.println("Termina com dança? " +musica.endsWith("dança"));
+		System.out.println("Caractere na posição 3: " +musica.charAt(3));
+		System.out.println("Index da letra \"a\": " + musica.indexOf("a"));
+		System.out.println("Index da palavra \"hoje\": " +musica.indexOf("hoje"));
+		System.out.println("Index do último \"o\": " +musica.lastIndexOf("o")); 
+		
+		System.out.println("+++++++++++++++++++++++++++++++++++++");
+		
+		String acima = new String("Mas quando sempre é sempre nunca");
+		
+		System.out.println(acima.replace("M", "m"));
+		System.out.println(acima.replace("quando", "qndo"));
+		
+		System.out.println(acima.split(" ").length);
+		System.out.println(Arrays.toString(acima.split(" ")));
+	
 	}
 
 }
